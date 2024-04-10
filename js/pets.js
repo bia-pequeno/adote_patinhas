eb6035e28c0215662f887a3cd8849691286d6e76
Reginaldo <ferreirajuniorreginaldo@gmail.com>
2024-04-10 18:35:04 -0300

    add pagina de pets

---
+++ js/pets.js
@@ -0,0 +1,163 @@
+// -----cachorro
+function show_pan() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("pandora").style.visibility = "visible";
+}
+function some_pan() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("pandora").style.visibility = "hidden";
+}
+
+function show_paç() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("paçoca").style.visibility = "visible";
+}
+function some_paç() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("paçoca").style.visibility = "hidden";
+}
+
+function show_don() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("donatello").style.visibility = "visible";
+}
+function some_don() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("donatello").style.visibility = "hidden";
+}
+
+function show_dan() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("dandara").style.visibility = "visible";
+}
+function some_dan() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("dandara").style.visibility = "hidden";
+}
+
+function show_apo() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("apollo").style.visibility = "visible";
+}
+function some_apo() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("apollo").style.visibility = "hidden";
+}
+
+function show_sir() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("sirius").style.visibility = "visible";
+}
+function some_sir() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("sirius").style.visibility = "hidden";
+}
+
+function show_luk() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("luke").style.visibility = "visible";
+}
+function some_luk() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("luke").style.visibility = "hidden";
+}
+
+function show_reg() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("reginaldo").style.visibility = "visible";
+}
+function some_reg() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("reginaldo").style.visibility = "hidden";
+}
+
+function show_tob() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("toby").style.visibility = "visible";
+}
+function some_tob() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("toby").style.visibility = "hidden";
+}
+// ----gato
+function show_ael() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("aelin").style.visibility = "visible";
+}
+function some_ael() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("aelin").style.visibility = "hidden";
+}
+
+function show_ala() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("alan").style.visibility = "visible";
+}
+function some_ala() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("alan").style.visibility = "hidden";
+}
+
+function show_fai() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("faisca").style.visibility = "visible";
+}
+function some_fai() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("faisca").style.visibility = "hidden";
+}
+
+function show_garf() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("garfield").style.visibility = "visible";
+}
+function some_garf() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("garfield").style.visibility = "hidden";
+}
+
+function show_the() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("theodore").style.visibility = "visible";
+}
+function some_the() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("theodore").style.visibility = "hidden";
+}
+
+function show_mic() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("michelin").style.visibility = "visible";
+}
+function some_mic() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("michelin").style.visibility = "hidden";
+}
+
+function show_dom() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("dom_pixote").style.visibility = "visible";
+}
+function some_dom() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("dom_pixote").style.visibility = "hidden";
+}
+
+function show_jud() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("judo").style.visibility = "visible";
+}
+function some_jud() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("judo").style.visibility = "hidden";
+}
+
+function show_gar() {
+  document.getElementById("tudo").style.visibility = "visible";
+  document.getElementById("garcia").style.visibility = "visible";
+}
+function some_gar() {
+  document.getElementById("tudo").style.visibility = "hidden";
+  document.getElementById("garcia").style.visibility = "hidden";
+}
+//-----------------------------
